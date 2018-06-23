c22980110976c8e5200fa273eb32f8978daaa4fb
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    x2: Build only system HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e

--- device.mk
+++ device.mk
@@ -76,8 +76,8 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0_system
 
 # IMS
 PRODUCT_PACKAGES += \
