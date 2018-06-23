b8c83e33f37228a7b65456c350fc520ee74f3d18
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    x2: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device.mk
+++ device.mk
@@ -80,7 +80,9 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # IMS
 PRODUCT_PACKAGES += \
