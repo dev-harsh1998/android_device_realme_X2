e7b2ecee14e3c39b89897f71a09cb51c1feeedbc
Ethan Chen <intervigil@gmail.com>
2019-02-03 15:12:39 -0800

    x2: Disable verity and descriptor checking
    * avbtool takes a --flags argument, not --flag
    * avbtool provides an option to disable hashtree checking
    
    Change-Id: Ib57d8ebed69a0998005563a9268f9fc736cbeaf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,6 +151,7 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
 # Inherit from the proprietary version
