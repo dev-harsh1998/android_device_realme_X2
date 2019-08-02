ae8497ab7b7658ba63b9419b0fb1046ff7c8315c
Bruno Martins <bgcngm@gmail.com>
2019-08-02 14:13:42 +0100

    x2: Also compile system variant of vendor.display.config

--- device.mk
+++ device.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
     libvulkan \
     memtrack.sm6150 \
     vendor.display.config@1.7 \
+    vendor.display.config@1.7.vendor \
     vendor.qti.hardware.display.allocator@1.0-service
 
 # Doze
