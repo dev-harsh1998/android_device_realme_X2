a62d627a22905f400cb2d39c13bf5204a8d43cca
Bruno Martins <bgcngm@gmail.com>
2019-09-05 12:17:15 +0100

    x2: Define Soong namespaces in the common product makefile
    This is where it belongs.
    
    Change-Id: Ibea0e8fe5dbc26d8953a9735f0b38cf087805b76

--- device.mk
+++ device.mk
@@ -136,6 +136,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
