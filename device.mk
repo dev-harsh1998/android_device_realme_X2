1f09af76a3c81545cad80471a4f1bbdd8f28b4f8
LuK1337 <priv.luk@gmail.com>
2019-11-04 21:33:58 -0500

    x2: Finish up QCOM WFD

--- device.mk
+++ device.mk
@@ -457,9 +457,11 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libln \
+    libnl \
     libdisplayconfig.vendor \
-    libqdMetaData
+    libqdMetaData \
+    libqdMetaData.system \
+    libqdMetaData.vendor
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
