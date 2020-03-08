64dbbc701546e0401ac008d8513042cfa9cff63c
Joey Rizzoli <joey@lineageos.org>
2020-03-08 14:57:21 +0530

    x2: add support for touchscreen gestures via lineagehw
    * Also cleanup repeated boardconfig flags while we are at it.
    
    Change-Id: I40a656e90f0b6f2b4ae6b596d4c28cc383b7c9fa
    Signed-off-by: Joey Rizzoli <joey@lineageos.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,8 +79,6 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
-TARGET_HAS_HDR_DISPLAY := true
-TARGET_HAS_WIDE_COLOR_DISPLAY := true
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
@@ -145,8 +143,9 @@ BOARD_VNDK_VERSION := current
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 VENDOR_SECURITY_PATCH := 2020-01-05
 
-# Verified Boot
-BOARD_AVB_ENABLE := true
+# Lineage Hardware
+JAVA_SOURCE_OVERLAYS := \
+    org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
