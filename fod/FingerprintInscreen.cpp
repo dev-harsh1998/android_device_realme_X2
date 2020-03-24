b8aae7cc19b52bfef0eb67d52388f817accdfcaa
dev-harsh1998 <dev-harsh1998@hotmail.com>
2020-03-24 17:45:46 +0530

    x2: fod: add dim calculations and delay to fix sensitivity
    Signed-off-by: dev-harsh1998 <dev-harsh1998@hotmail.com>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -21,7 +21,7 @@
 #include <android-base/logging.h>
 #include <fstream>
 #include <cmath>
-
+#include <thread>
 
 /* Hardcoded stuffs */
 #define FP_PRESS_PATH "/sys/kernel/oppo_display/notify_fppress"
@@ -61,7 +61,7 @@ namespace inscreen {
 namespace V1_0 {
 namespace implementation {
 
-FingerprintInscreen::FingerprintInscreen() {
+FingerprintInscreen::FingerprintInscreen():mFingerPressed{false} {
 }
 
 Return<int32_t> FingerprintInscreen::getPositionX() {
@@ -85,13 +85,20 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    mFingerPressed = true;
     set(DIMLAYER_PATH, FP_BEGIN);
     set(HBM_PATH, FP_BEGIN);
-    set(FP_PRESS_PATH, FP_BEGIN);
+    std::thread([this]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(120));
+        if (mFingerPressed) {
+            set(FP_PRESS_PATH, FP_BEGIN);
+        }
+    }).detach();
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    mFingerPressed = false;
     set(FP_PRESS_PATH, FP_ENDIT);
     set(DIMLAYER_PATH, FP_ENDIT);
     set(HBM_PATH, FP_ENDIT);
@@ -124,7 +131,8 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
 }
 
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
-    return 0;
+    return(int32_t)((brightness > 498) ? (255 * (1.0 - pow(brightness / 2047.0 * 430.0 / 600.0, 0.455))):
+            (255 * (1.0 - pow(brightness / 1605.0, 0.455)))); 
 }
 
 Return<bool> FingerprintInscreen::shouldBoostBrightness() {
