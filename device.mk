132a28ced3d43db352fec51066f4327f27723e2a
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-04-05 13:50:13 +0530

    x2: enable PRODUCT_ENFORCE_RRO_TARGETS
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Properties
 -include $(LOCAL_PATH)/vendor_props.mk
 
