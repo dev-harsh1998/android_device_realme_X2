591fac95116b5d7acab7e8fa9258b07eedb94705
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-04-05 13:27:39 +0530

    x2: Build bluetooth from source
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device.mk
+++ device.mk
@@ -111,6 +111,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    libbthost_if
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
