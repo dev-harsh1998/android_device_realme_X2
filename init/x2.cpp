55cf84b7b1dd0f44f9dc145f279959d91c80c869
dev-harsh1998 <dev-harsh1998@hotmail.com>
2020-04-10 22:44:51 +0530

    x2: add init to set device identifiers
    Signed-off-by: dev-harsh1998 <dev-harsh1998@hotmail.com>

---
+++ init/x2.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2020 LineageOS Project
+ * Copyright (C) 2020 Harshit Jain
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <vector>
+#include <string>
+#include <fstream>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include <android-base/properties.h>
+#include <android-base/logging.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+struct x2_props
+{
+    std::string build_description;
+    std::string build_fingerprint;
+    std::string device_build;
+    std::string product_device;
+};
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+};
+
+bool isCN()
+{
+    // Get region
+    std::ifstream infile("/proc/oppoVersion/region");
+    std::string region;
+    bool ret = false;
+    getline(infile, region);
+    if (!region.compare("China"))
+        ret = true;
+    return ret;
+}
+
+bool hasNFC()
+{
+    // Check NFC
+    std::ifstream infile("/proc/touchpanel/NFC_CHECK");
+    std::string check;
+    bool ret = false;
+    getline(infile, check);
+    if (!check.compare("SUPPORTED"))
+        ret = true;
+    return ret;
+}
+
+// Make life easier
+bool isRMX1993()
+{
+    return (!isCN() && hasNFC());
+}
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    prop_info *pi;
+    pi = (prop_info *)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void setRMX(unsigned int variant)
+{
+    x2_props prop[3] = {};
+
+    //RMX1991
+    prop[0] = {
+        "realme/RMX1991/RMX1991CN:9/PKQ1.190630.001/1575615457:user/release-keys",
+        "sm6150-user 9 PKQ1.190630.001 eng.root.20191226.143855 release-keys",
+        "RMX1991",
+        "RMX1991CN",
+    };
+
+    //RMX992
+    prop[1] = {
+        "realme/RMX1992/RMX1992L1:9/PKQ1.190630.001/1578551002:user/release-keys",
+        "sm6150-user 9 PKQ1.190630.001 eng.root.20200118.175901 release-keys",
+        "RMX1992",
+        "RMX1992L1",
+    };
+
+    //RMX1993
+    prop[2] = {
+        "realme/RMX1993EEA/RMX1993L1:9/PKQ1.190630.001/1581056768:user/release-keys",
+        "sm6150-user 9 PKQ1.190630.001 eng.root.20200208.185557 release-keys",
+        "RMX1993",
+        "RMX1993L1",
+    };
+
+    const auto set_ro_build_prop = [](const std::string &source,
+                                      const std::string &prop, const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+                                        const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    property_override("ro.build.description", prop[variant].build_fingerprint.c_str());
+    property_override("ro.build.product", prop[variant].product_device.c_str());
+    for (const auto &source : ro_props_default_source_order)
+    {
+        set_ro_build_prop(source, "fingerprint", prop[variant].build_description.c_str());
+        set_ro_product_prop(source, "device", prop[variant].product_device.c_str());
+        set_ro_product_prop(source, "model", prop[variant].device_build.c_str());
+    }
+}
+
+void vendor_load_properties()
+{
+    if (isRMX1993())
+    {
+        setRMX(2); //RMX1993
+    }
+    else if (!isCN())
+    {
+        setRMX(1); //RMX1992
+    }
+    else
+    {
+        setRMX(0); //RMX1991
+    }
+}
