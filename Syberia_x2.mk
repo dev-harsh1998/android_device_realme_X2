5af12deda51503590f28c16911b44a06383b7b6a
slaughtermc <47281879+slaughtermc@users.noreply.github.com>
2020-05-01 18:12:27 +0530

    Update and rename lineage_x2.mk to Syberia_x2.mk

--- Syberia_x2.mk
+++ Syberia_x2.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/realme/x2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Syberia stuff.
+$(call inherit-product, vendor/syberia/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := syberia_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
@@ -18,4 +18,4 @@ PRODUCT_MANUFACTURER := realme
 
 BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:9/PKQ1.190630.001/1578551002:user/release-keys"
 
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
+PRODUCT_GMS_CLIENTID_BASE := android-realme
