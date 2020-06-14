6fdd72b7d758aa5cea88c80f11cd1c204904dccd
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-06-14 20:35:59 +0530

    x2: Build missing ANT+ packages
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device.mk
+++ device.mk
@@ -80,6 +80,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+
 # ATRACE_HAL
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
