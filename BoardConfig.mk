2806541b5210ef35d631977af8824c21fd773aaf
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-06-14 23:38:26 +0530

    x2: Cleanup sepolicy
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,9 +196,9 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Sepolicy
+TARGET_SEPOLICY_DIR := msmsteppe
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
 # Treble
 VENDOR_SECURITY_PATCH := 2020-01-05
