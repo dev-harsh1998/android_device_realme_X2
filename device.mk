b4cb549139cf8dc5c2d9a5257e41fb5e406b226e
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-06-17 23:49:51 +0530

    x2: Build vendor.display.config@1.9
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device.mk
+++ device.mk
@@ -149,6 +149,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     memtrack.sm6150 \
+    vendor.display.config@1.9 \
     vendor.qti.hardware.display.allocator-service
 
 # Display interfaces
