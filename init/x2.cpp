aafc7fc79ea719f2caf79cadb0a895d0d643ca9b
Harshit Jain <god@harsh1998.me>
2020-06-24 03:05:41 +0530

    x2: init: update fingerprint from  RMX1992EX_11.C.09

--- init/x2.cpp
+++ init/x2.cpp
@@ -100,8 +100,8 @@ void setRMX(const unsigned int variant)
 
     //RMX992
     prop[1] = {
-        "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1587908485:user/release-keys",
-        "unknown-user 10 QKQ1.191201.002 eng.root.20200518.233658 release-keys",
+        "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1589968374:user/release-keys",
+        "unknown-user 10 QKQ1.191201.002 eng.root.20200617.225734 release-keys",
         "RMX1992",
         "RMX1992L1",
     };
