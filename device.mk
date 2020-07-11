752f9ae81573d20baa6e6565d1fcdf1fb72ed287
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-07-11 12:04:37 +0530

    x2: build liveDisplay
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.x2 \
     lights.sm6150
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
