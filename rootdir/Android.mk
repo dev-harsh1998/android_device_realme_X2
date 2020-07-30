8a40ad687e9b4220a1b1025f746bc048ccea0e02
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-07-30 16:12:57 +0530

    x2: rootdir changes for artemis
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -195,6 +195,13 @@ include $(BUILD_PREBUILT)
 
 # Common init scripts
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.artemis.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.artemis.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.devicesetting.rc
