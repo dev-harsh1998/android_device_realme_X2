85cbcbdccc3734d2fbeecdb550d03052894b5787
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-07-31 18:53:39 +0530

    Revert "x2: Don't set aod light mode"
    This reverts commit 7a6683902e9adc2c049b38ef2786c27cd75c9991.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -27,6 +27,7 @@
 #define FP_PRESS_PATH "/sys/kernel/oppo_display/notify_fppress"
 #define DIMLAYER_PATH "/sys/kernel/oppo_display/dimlayer_hbm"
 #define NOTIFY_BLANK_PATH "/sys/kernel/oppo_display/notify_panel_blank"
+#define AOD_MODE_PATH "/sys/kernel/oppo_display/aod_light_mode_set"
 #define DOZE_STATUS "/proc/touchpanel/DOZE_STATUS"
 #define X_POS 442
 #define Y_POS 1969
@@ -111,6 +112,7 @@ Return<void> FingerprintInscreen::onShowFODView() {
     if(get(DOZE_STATUS, FP_ENDIT)) {
     isDreamState = true;
     set(NOTIFY_BLANK_PATH, FP_BEGIN);
+    set(AOD_MODE_PATH, FP_BEGIN);
     } else {
     isDreamState = false;
     set(DIMLAYER_PATH, FP_BEGIN);
