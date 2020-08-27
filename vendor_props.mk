3a953cfa303a65ac2ad554c022351f704850232e
Harshit Jain <god@hyper-labs.tech>
2020-08-27 18:01:50 +0530

    X2: sync display flags and offsets with sunfish

--- vendor_props.mk
+++ vendor_props.mk
@@ -68,10 +68,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-   debug.sf.early_phase_offset_ns=500000 \
-   debug.sf.early_app_phase_offset_ns=500000 \
+   debug.sf.early_phase_offset_ns=11600000 \
+   debug.sf.early_app_phase_offset_ns=11600000 \
    debug.sf.early_gl_phase_offset_ns=3000000 \
-   debug.sf.early_gl_app_phase_offset_ns=15000000
+   debug.sf.early_gl_app_phase_offset_ns=15000000 \
+   debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
