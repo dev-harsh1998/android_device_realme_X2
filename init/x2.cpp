f683b9bcab1d39ee88a51e248b5a11b56aac5120
TH3GUNN3R <bibhuti.gamer@gmail.com>
2020-09-01 16:46:39 +0000

    x2:Update Init From @dev-harsh1998 tree

--- init/x2.cpp
+++ init/x2.cpp
@@ -22,8 +22,11 @@ using android::init::property_set;
 
 struct x2_props
 {
+    std::string build_description;
+    std::string build_fingerprint;
     std::string device_build;
     std::string product_device;
+    std::string device_spl;
 };
 
 std::vector<std::string> ro_props_default_source_order = {
@@ -84,26 +87,35 @@ void load_nfc_props()
     property_set("persist.sys.nfc.antenna_area", "bottom");
 }
 
-void setRMX(unsigned int variant)
+void setRMX(const unsigned int variant)
 {
     x2_props prop[3] = {};
 
     //RMX1991
     prop[0] = {
-        "Pixel 4 XL",
-        "coral",
+        "realme/RMX1991/RMX1991CN:10/QKQ1.191201.002/1592194586:user/release-keys",
+        "unknown-user 10 QKQ1.191201.002 eng.root.20200716.002633 release-keys",
+        "RMX1991",
+        "RMX1991CN",
+        "2020-07-05",
     };
 
     //RMX992
     prop[1] = {
-        "Pixel 4 XL",
-        "coral",
+        "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1592194586:user/release-keys",
+        "unknown-user 10 QKQ1.191201.002 eng.root.20200723.192038 release-keys",
+        "RMX1992",
+        "RMX1992L1",
+        "2020-07-05",
     };
 
     //RMX1993
     prop[2] = {
-        "Pixel 4 XL",
-        "coral",
+        "realme/RMX1993EEA/RMX1993L1:10/QKQ1.191201.002/1587908485:user/release-keys",
+        "unknown-user 10 QKQ1.191201.002 eng.root.20200518.233251 release-keys",
+        "RMX1993",
+        "RMX1993L1",
+        "2020-06-05",
     };
 
     const auto set_ro_build_prop = [](const std::string &source,
@@ -118,12 +130,26 @@ void setRMX(unsigned int variant)
         property_override(prop_name.c_str(), value.c_str(), false);
     };
 
+    property_override("ro.build.description", prop[variant].build_fingerprint.c_str());
     property_override("ro.build.product", prop[variant].product_device.c_str());
     for (const auto &source : ro_props_default_source_order)
     {
+        set_ro_build_prop(source, "fingerprint", prop[variant].build_description.c_str());
         set_ro_product_prop(source, "device", prop[variant].product_device.c_str());
         set_ro_product_prop(source, "model", prop[variant].device_build.c_str());
+        set_ro_product_prop(source, "name", prop[variant].device_build.c_str());
     }
+
+    // All 3 variants get upadates at different time so let's handle it here.
+    property_override("ro.build.version.security_patch", prop[variant].device_spl.c_str());
+
+    // RMX1993 has different ptoduct name due to oversea variants further being divided into spain and europe
+    if (variant == 2)
+        for (const auto &source : ro_props_default_source_order)
+            set_ro_product_prop(source, "name", "RMX1993EEA"); //override name props again
+
+    //override again to be sure that it gets set.
+    property_override("ro.build.fingerprint", prop[variant].build_description.c_str(), false);
 
     // Load NFC properties only on RMX199{1:3}
     if (variant == 2 || variant == 0)
