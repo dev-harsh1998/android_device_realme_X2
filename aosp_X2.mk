06b026c73c08a2e5f9f23631deec30ebe9466a31
Harshit Jain <god@hyper-labs.tech>
2020-09-11 21:10:37 +0530

    X2: migrate product makefiles for aosp build

--- aosp_X2.mk
+++ aosp_X2.mk
@@ -1,16 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X2
+PRODUCT_NAME := aosp_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
