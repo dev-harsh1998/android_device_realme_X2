4105136d4ac1db3f5a7a2cbb4fa23619fab5b203
Harshit Jain <god@hyper-labs.tech>
2020-09-13 20:51:35 +0530

    X2: build minimal sepolicy for now
    Signed-off-by: Harshit Jain <god@hyper-labs.tech>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,15 +147,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    $(DEVICE_PATH)/sepolicy/private \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
-
-SELINUX_IGNORE_NEVERALLOWS := true
+    $(DEVICE_PATH)/sepolicy/private
 
 # System-as-root
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
