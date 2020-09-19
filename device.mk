300a2f7f72ae4fa98e590cdab204ee00bc31571b
Harshit Jain <god@hyper-labs.tech>
2020-09-19 17:22:43 +0530

    X2: remove unneeded inscreen perm
    * Aosp doesn't build lineage-sdk

--- device.mk
+++ device.mk
@@ -89,10 +89,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
-# Lineage Specific perms
-PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
