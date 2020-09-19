845b8283c0662011cbd5875e830f10ed51d0499e
Harshit Jain <god@hyper-labs.tech>
2020-09-19 17:28:21 +0530

    X2: override adb secure on eng and rescue party on userdebug builds
    * Let us debug in peace.
    
    Signed-off-by: Harshit Jain <god@hyper-labs.tech>

--- product_prop.mk
+++ product_prop.mk
@@ -1,3 +1,18 @@
+# Adb
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it in /product on eng builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
+
+# Disable Rescue Party on userdebug & eng build
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+endif
+
 # Priv-app permission
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
