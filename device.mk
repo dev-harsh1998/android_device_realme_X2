59ff8f28784c48aff5d606bec928ba9d5b4f740a
Harshit Jain <god@hyper-labs.tech>
2020-09-20 20:19:05 +0530

    X2: audio: import and bind mount stock mixer paths

--- device.mk
+++ device.mk
@@ -53,7 +53,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info_oppo_19771.xml:system/etc/audio_platform_info_oppo_19771.xml
+    $(LOCAL_PATH)/audio/audio_platform_info_oppo_19771.xml:system/etc/audio_platform_info_oppo_19771.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_19771.xml:system/etc/mixer_paths_19771.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
