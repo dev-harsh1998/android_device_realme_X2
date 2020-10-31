9aa65f1789d204042db08bfb0e2e8b5686f744cf
RiasChan12 <70882674+RiasChan12@users.noreply.github.com>
2020-10-31 14:39:02 +0700

    Update aosp_X2.mk
    Rebase

--- aosp_X2.mk
+++ aosp_X2.mk
@@ -4,15 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, device/realme/X2/device.mk)
+$(call inherit-product, device/realme/realme_trinket/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X2
-PRODUCT_DEVICE := X2
+PRODUCT_NAME := aosp_realme_trinket
+PRODUCT_DEVICE := realme_trinket
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := realme X2
+PRODUCT_MODEL := realme_trinket
 PRODUCT_MANUFACTURER := realme
 
-BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1592194586:user/release-keys"
+BUILD_FINGERPRINT := "trinket-user-10-QKQ1.200209.002--release-keys"
 
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
+PRODUCT_GMS_CLIENTID_BASE := android-realme
