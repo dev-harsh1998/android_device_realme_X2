8cea0dcdce9df382e1fc8793cef73ebb43241f5b
TheScarastic <warabhishek@gmail.com>
2020-12-29 09:04:41 +0000

    [dnm]X2: copy android.software.controls permission

--- device.mk
+++ device.mk
@@ -104,7 +104,8 @@ PRODUCT_PACKAGES += \
 #Hardware
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml \
+    frameworks/native/data/etc/android.software.controls.xml:system/etc/permissions/android.software.controls.xml
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
