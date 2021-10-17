7623907c3adbd9b96c542e4f801ad05fcbccac36
Harshit Jain <harshit@hyperteam.co.in>
2021-10-17 17:02:39 +0000

    [TEMP]X2: disable lineage trust hal for time being

--- device.mk
+++ device.mk
@@ -188,8 +188,8 @@ PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
 # Trust HAL
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
+#PRODUCT_PACKAGES += \
+#    lineage.trust@1.0-service
 
 # WiFi Display
 PRODUCT_PACKAGES += \
