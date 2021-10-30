24ed3989a9ca10f7e00f93f81259bb88e1763f75
Harshit Jain <harshit@hyperteam.co.in>
2021-10-30 04:34:32 +0000

    X2: time to go back to aosp builds

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X2.mk
+    $(LOCAL_DIR)/aosp_X2.mk
--- aosp_X2.mk
+++ aosp_X2.mk
@@ -6,11 +6,8 @@
 
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X2
+PRODUCT_NAME := aosp_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
