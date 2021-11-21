e4f7e64e3be6a384ad05ea6b944be8ae48adc4e6
Vaisakh Murali <mvaisakh@statixos.com>
2021-11-21 20:12:45 +0100

    X2: Copy libprotobuf-cpp-full.so and libprotobuf-cpp-lite.so VNDK prebuilt blobs
    * linker  : CANNOT LINK EXECUTABLE /vendor/bin/hw/android.hardware.drm@1.2-service.clearkey: library libprotobuf-cpp-lite.so not found: needed by main executable

--- device.mk
+++ device.mk
@@ -16,6 +16,12 @@ $(call inherit-product, vendor/realme/X2/X2-vendor.mk)
 PRODUCT_TARGET_VNDK_VERSION := 29
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so 
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
