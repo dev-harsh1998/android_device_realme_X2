f130c2720ff2ff260decabc43503c49646aa8797
Retypolkg <retypolkg2021@gmail.com>
2021-11-24 07:51:37 +0100

    Revert "x2: move safailnet to vendor"
    This reverts commit ee8f7c81bf1ac39ce4ac1bfcc6b7d1f2e168b497.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -258,7 +258,7 @@ LOCAL_MODULE       := init.safailnet.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
