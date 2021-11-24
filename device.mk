7a66fd80b147e5e89fd27df47f160b35078abf5b
Davide Garberi <dade.garberi@gmail.com>
2021-11-24 15:59:30 +0100

    X2: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b

--- device.mk
+++ device.mk
@@ -212,7 +212,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
