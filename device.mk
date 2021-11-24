c272021786026e7efb59018ea902a26b3213536d
Retypolkg <retypolkg2021@gmail.com>
2021-11-24 08:24:20 +0100

    X2: Nuke devicesettings and doze for now

--- device.mk
+++ device.mk
@@ -160,11 +160,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer@2.0.vendor \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    RealmeParts \
-    devicesettings
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
