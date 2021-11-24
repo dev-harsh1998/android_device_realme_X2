274198f47c4a9b6aef420008f086bddf50686eca
Retypolkg <retypolkg2021@gmail.com>
2021-11-24 16:11:14 +0100

    X2: Nuke Lineage Touch hal
    * nuke trust too

--- device.mk
+++ device.mk
@@ -434,14 +434,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sm6150
 
-# Touch
-PRODUCT_PACKAGES += \
-    lineage.touch@1.0-service.x2
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
