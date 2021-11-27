d71837ea9b2d2c642b9835ab153fb38a587cb846
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-11-27 12:32:36 +0100

    X2: Provide android.hardware.radio@1.4 on vendor
    * Also build its vendor variant
    
    11-26 16:03:44.669  4620  4620 W HidlServiceManagement: Waited one second for android.hardware.radio@1.4::IRadio/slot1
    11-26 16:03:44.670   501   501 I hwservicemanager: Since android.hardware.radio@1.4::IRadio/slot1 is not registered, trying to start it as a lazy HAL.
    11-26 16:03:44.670  4620  4620 I HidlServiceManagement: getService: Trying again for android.hardware.radio@1.4::IRadio/slot1...
    11-26 16:03:44.670  5522  5522 F libc    : Fatal signal 6 (SIGABRT), code -1 (SI_QUEUE) in tid 5522 (vendor.qti.medi), pid 5522 (vendor.qti.medi)
    11-26 16:03:44.672   501  5525 W libc    : Unable to set property "ctl.interface_start" to "android.hardware.radio@1.4::IRadio/slot1": error code: 0x20
    11-26 16:03:44.672   501  5525 I hwservicemanager: Tried to start android.hardware.radio@1.4::IRadio/slot1 as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.

--- device.mk
+++ device.mk
@@ -351,6 +351,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
+    android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2 \
     android.hardware.secure_element@1.0 \
     libjson \
