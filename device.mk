10402c0e7afca81078354a39d0f54178f2feb10a
maitreya29 <maitreyapatni30@gmail.com>
2021-11-27 12:38:32 +0100

    X2: Build android.hardware.radio.deprecated@1.0
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.radio.deprecated@1.0.so" not found

--- device.mk
+++ device.mk
@@ -356,6 +356,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.secure_element@1.0 \
     android.hardware.secure_element@1.0.vendor \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0.vendor \
     libjson \
     librmnetctl \
     libxml2 \
