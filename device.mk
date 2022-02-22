4fa44c2631542896ce53c1b8c967a15db0ba0aa7
baolong24 <1667375308@qq.com>
2022-02-22 17:55:30 +0100

    X2: Build android.hardware.bluetooth.audio@2.0-impl
    Signed-off-by: Retypolkg <retypolkg2021@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
