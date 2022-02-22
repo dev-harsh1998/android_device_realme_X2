0764514920615204aa6a1f791a272f97053ea3bb
SebaUbuntu <barezzisebastiano@gmail.com>
2022-02-22 11:56:36 +0100

    X2: Uprev clearkey DRM HIDL to 1.4

--- device.mk
+++ device.mk
@@ -186,10 +186,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
     android.hardware.drm@1.2 \
-    android.hardware.drm@1.2.vendor
-
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.4-service.clearkey
+    
 # Parts
 PRODUCT_PACKAGES += \
     RealmeProximityHelper \
