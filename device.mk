4970e1aef3d925468977fc976ac19534d418a52c
LuK1337 <priv.luk@gmail.com>
2022-02-22 11:41:28 +0100

    X2: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.

--- device.mk
+++ device.mk
@@ -91,12 +91,8 @@ PRODUCT_PACKAGES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
+    AntHalService
+    
 # ATRACE_HAL
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
