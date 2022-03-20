14f4fe42c284cdd1b760c0489b359445d65ca587
Retypolkg <retypolkg2021@gmail.com>
2022-03-20 12:18:13 +0100

    X2: Cleanup seccomps
    * https://github.com/jebac-motorole-team/device_realme_X2/commit/e6ce932e7badbce0f2ec42f2883df2efe033f908
    
    Signed-off-by: Retypolkg <retypolkg2021@gmail.com>

--- device.mk
+++ device.mk
@@ -424,12 +424,7 @@ PRODUCT_PACKAGES += \
     
 # RenderScript
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl 
-
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    android.hardware.renderscript@1.0-impl
 
 # Sensors
 PRODUCT_PACKAGES += \
