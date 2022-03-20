14bb8ef070be63d95825289393fb042f47ae0a4c
Retypolkg <retypolkg2021@gmail.com>
2022-03-20 11:43:59 +0100

    X2: Update qti configs to RUI 2.0
    * From qssi-user 11 RKQ1.201112.002 root05271736 release-keys (RMX1991 F.26)
    * Also move hotword configs to /hotword
    
    Signed-off-by: Retypolkg <retypolkg2021@gmail.com>

--- device.mk
+++ device.mk
@@ -256,8 +256,8 @@ PRODUCT_COPY_FILES += \
     
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/hotword/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/configs/hotword/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IMS
 PRODUCT_PACKAGES += \
